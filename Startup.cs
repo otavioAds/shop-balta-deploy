1351db3b061fea3e2f2c0a328e82890af12499a0
Carlos <carlos.vieira@vuori.com.br>
2020-09-22 22:41:43 -0300

    Correção Banco de dados 3

--- Startup.cs
+++ Startup.cs
@@ -70,9 +70,10 @@ namespace Shop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-            app.UseDeveloperExceptionPage();
-
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseHttpsRedirection();
 
